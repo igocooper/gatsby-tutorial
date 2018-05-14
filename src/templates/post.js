6de526d059cb2e8790f1979edb024814c4b9974b
Oleg Lytvyn <igocooper@zeoalliance.com>
2018-05-14 14:14:35 +0300

    gatsby post part 2

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+ 
 
 export default function Template({ data }) {
     const { markdownRemark: post } = data;
@@ -7,11 +8,12 @@ export default function Template({ data }) {
     return (
         <div>
             <h1>{post.frontmatter.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: post.html }} />
         </div>
     )
 };
 
-export const postQuery = grapqhl`
+export const postQuery = graphql`
     query BlogPostByPath($path: String!) {
         markdownRemark(frontmatter: { path: { eq: $path} }) {
             html
